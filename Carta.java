eba8bdc4cb14a1c2114f1d2ffd90b75f46bb1f5e
Carlos Moro <carlos.moro.dam1@iessanandres.com>
2017-01-19 12:40:22 +0100

    método que indica el número y palo de la carta que se introduce por teclado

---
+++ Carta.java
@@ -0,0 +1,92 @@
+
+/**
+ * Write a description of class Carta here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Carta
+{
+    // instance variables - replace the example below with your own
+    private int valor;
+    private int palo;
+    
+    /**
+     * Constructor for objects of class Carta
+     */
+    public Carta(int valor, int palo)
+    {
+        this.valor = valor;
+        this.palo = palo;
+    }
+
+    public String toString()
+    {
+        String nombreCarta = "";
+        if(palo >= 0 && palo <= 3 || valor >= 1 && valor <= 7 || valor >= 10 && valor <= 12)
+        {
+            if (valor == 1)
+            {
+                nombreCarta = "as";
+            }
+            else if (valor == 2)
+            {
+                nombreCarta = "2";
+            }
+            else if (valor == 3)
+            {
+                nombreCarta = "3";
+            }
+            else if (valor == 4)
+            {
+                nombreCarta = "4";
+            }
+            else if (valor == 5)
+            {
+                nombreCarta = "5";
+            }
+            else if (valor == 6)
+            {
+                nombreCarta = "6";
+            }
+            else if (valor == 7)
+            {
+                nombreCarta = "7";
+            }
+            else if (valor == 10)
+            {
+                nombreCarta = "sota";
+            }
+            else if (valor == 11)
+            {
+                nombreCarta = "caballo";
+            }
+            else if (valor == 12)
+            {
+                nombreCarta = "rey";
+            }
+            if (palo == 0)
+            {
+                nombreCarta += " de oros";
+            }
+            else if (palo == 1)
+            {
+                nombreCarta += " de copas";
+            }
+            else if (palo == 2)
+            {
+                nombreCarta += " de espadas";
+            }
+            else if (palo == 3)
+            {
+                nombreCarta += " de bastos";
+            }
+        
+        }
+        else
+        {
+            System.out.println("valores erroneos");
+        }
+        return nombreCarta;
+    }
+}
