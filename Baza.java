ed8a744fab8e2a5755d309372dc66ad94fd5c8a6
Carlos Moro <carlos.moro.dam1@iessanandres.com>
2017-02-02 10:42:11 +0100

    constructor que admita dos parametros

---
+++ Baza.java
@@ -0,0 +1,13 @@
+
+public class Baza
+{
+    private int numeroJugadores;
+    private int paloQuePinta;
+    
+    public Baza(int numeroJugadores, int paloQuePinta)
+    {
+        this.numeroJugadores = 0;
+        this.paloQuePinta = 0;
+    }
+
+}
