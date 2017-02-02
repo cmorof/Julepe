2777228c5800e257fb5f57634fcbe963ca1cfe4e
Carlos Moro <carlos.moro.dam1@iessanandres.com>
2017-02-02 11:12:24 +0100

    constructor corregido

--- Baza.java
+++ Baza.java
@@ -1,13 +1,22 @@
+import java.util.ArrayList;
 
 public class Baza
 {
     private int numeroJugadores;
     private int paloQuePinta;
+    private Carta[] baza;
     
     public Baza(int numeroJugadores, int paloQuePinta)
     {
-        this.numeroJugadores = 0;
-        this.paloQuePinta = 0;
+        numeroJugadores = 0;
+        paloQuePinta = 0;
+        baza = new Carta[numeroJugadores];
     }
-
+    
+    public void addCarta(Carta cartaQueTiraUnJugador, String nombreJugador)
+    {
+        int i = 0;
+        baza[i] = cartaQueTiraUnJugador;
+        i++;
+    }
 }
